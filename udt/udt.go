22f6ed8fbdc7a8301418dcbbbab96e5b3ce7ed97
Lawliet-Chan <1576710154@qq.com>
2025-11-05 14:30:32 +0800

    Native Token

--- udt/udt.go
+++ udt/udt.go
@@ -17,3 +17,12 @@ type ChainToken struct {
 	ChainURL     string `json:"chain_url"`
 	TokenAddress []byte `json:"token_address"`
 }
+
+var NativeToken = UDT{
+	Name:          "JingChou",
+	Description:   "JingChou Chain Native Token",
+	OriginalToken: nil,
+	Total:         new(big.Int).SetUint64(1000000000000000000),
+	Locked:        new(big.Int).SetUint64(0),
+	Issued:        new(big.Int).SetUint64(1000000000000000),
+}
